bfb6c46743aa3888f97522953e02caadfd686dea
shijiang.guo <shijiang@192.168.0.106>
2023-10-18 22:46:04 +0800

    contract fix

--- src/contracts/interface/IPausable.sol
+++ src/contracts/interface/IPausable.sol
@@ -5,45 +5,23 @@ import "../interface/IPauserRegistry.sol";
 
 
 interface IPausable {
-    /// @notice Emitted when the `pauserRegistry` is set to `newPauserRegistry`.
     event PauserRegistrySet(IPauserRegistry pauserRegistry, IPauserRegistry newPauserRegistry);
 
-    /// @notice Emitted when the pause is triggered by `account`, and changed to `newPausedStatus`.
     event Paused(address indexed account, uint256 newPausedStatus);
 
-    /// @notice Emitted when the pause is lifted by `account`, and changed to `newPausedStatus`.
     event Unpaused(address indexed account, uint256 newPausedStatus);
 
-    /// @notice Address of the `PauserRegistry` contract that this contract defers to for determining access control (for pausing).
     function pauserRegistry() external view returns (IPauserRegistry);
 
-    /**
-     * @notice This function is used to pause an EigenLayer contract's functionality.
-     * It is permissioned to the `pauser` address, which is expected to be a low threshold multisig.
-     * @param newPausedStatus represents the new value for `_paused` to take, which means it may flip several bits at once.
-     * @dev This function can only pause functionality, and thus cannot 'unflip' any bit in `_paused` from 1 to 0.
-     */
     function pause(uint256 newPausedStatus) external;
 
-    /**
-     * @notice Alias for `pause(type(uint256).max)`.
-     */
     function pauseAll() external;
 
-    /**
-     * @notice This function is used to unpause an EigenLayer contract's functionality.
-     * It is permissioned to the `unpauser` address, which is expected to be a high threshold multisig or governance contract.
-     * @param newPausedStatus represents the new value for `_paused` to take, which means it may flip several bits at once.
-     * @dev This function can only unpause functionality, and thus cannot 'flip' any bit in `_paused` from 0 to 1.
-     */
     function unpause(uint256 newPausedStatus) external;
 
-    /// @notice Returns the current paused status as a uint256.
     function paused() external view returns (uint256);
 
-    /// @notice Returns 'true' if the `indexed`th bit of `_paused` is 1, and 'false' otherwise
     function paused(uint8 index) external view returns (bool);
 
-    /// @notice Allows the unpauser to set a new pauser registry
     function setPauserRegistry(IPauserRegistry newPauserRegistry) external;
 }
