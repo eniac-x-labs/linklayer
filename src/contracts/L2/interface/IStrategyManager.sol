511209ef09614cafdbf703a23e185a16b2b167ac
Becky449 <rabnab12@gmail.com>
2023-11-08 21:47:56 +0300

    Edited Layer 1 and Layer 2 files

--- src/contracts/L2/interface/IStrategyManager.sol
+++ src/contracts/L2/interface/IStrategyManager.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.19;
 
 // import "./IStrategy.sol";
 // import "./ISlasher.sol";
-// import "./IDelegationManager.sol";
+import "./IDelegationManager.sol";
 // import "./IShadowxPodManager.sol";
 
 /**
